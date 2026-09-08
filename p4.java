b03ebde04d56a4a245fbebfec864cb4e3064a49b
shrujanc-cpu <shrujanc3@gmail.com>
2026-09-08 11:40:51 +0530

    Add files via upload

---
+++ p4.java
@@ -0,0 +1,9 @@
+class p4 {
+    public static void main(String[] args) {
+        int x = 30;
+        float y = x;   // int to float
+
+        System.out.println("Int: " + x);
+        System.out.println("Float: " + y);
+    }
+}
